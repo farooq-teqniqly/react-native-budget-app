3d107a477ea173bf461c9f0061bcea8d02b5e735
farooq <farooq@teqniqly.com>
2022-05-10 22:52:56 -0700

    wip

---
+++ budget-api/Api/Controllers/PayeeController.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Farooq Mahmud
+
+namespace Api.Controllers
+{
+	using System;
+	using System.Net;
+	using System.Threading.Tasks;
+	using Api.Models;
+	using AutoMapper;
+	using DataAccess;
+	using DataAccess.Entities;
+	using Microsoft.AspNetCore.Mvc;
+	using Microsoft.EntityFrameworkCore;
+
+	/// <summary>
+	/// A controller for working with payees..
+	/// </summary>
+	[Route("api/payees")]
+	[ApiController]
+	public class PayeeController : ControllerBase
+	{
+		private readonly DatabaseContext databaseContext;
+		private readonly IMapper mapper;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="PayeeController"/> class.
+		/// </summary>
+		/// <param name="databaseContext">The database context.</param>
+		/// <param name="mapper">The entity mapper.</param>
+		public PayeeController(DatabaseContext databaseContext, IMapper mapper)
+		{
+			this.databaseContext = databaseContext;
+			this.mapper = mapper;
+		}
+
+		/// <summary>
+		/// Gets all payees.
+		/// </summary>
+		/// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+		[HttpGet]
+		[Route("", Name = "GetPayees")]
+		[ProducesResponseType(typeof(GetPayeeResponse[]), (int)HttpStatusCode.OK)]
+		public async Task<IActionResult> GetPayees()
+		{
+			var payees = await this.databaseContext.Payees.ToArrayAsync();
+			var response = this.mapper.Map<Payee[], GetPayeeResponse[]>(payees);
+
+			return this.Ok(response);
+		}
+
+		/// <summary>
+		/// Creates a new payee.
+		/// </summary>
+		/// <param name="request">The request.</param>
+		/// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+		[HttpPost]
+		[ProducesResponseType(typeof(CreatePayeeResponse), (int)HttpStatusCode.Created)]
+		public async Task<IActionResult> CreatePayee([FromBody] CreatePayeeRequest request)
+		{
+			var payee = this.mapper.Map<CreatePayeeRequest, Payee>(request);
+			this.databaseContext.Payees.Add(payee);
+			await this.databaseContext.SaveChangesAsync();
+
+			var response = this.mapper.Map<Payee, CreatePayeeRequest>(payee);
+
+			return this.CreatedAtRoute("GetPayees", value: response);
+		}
+
+		/// <summary>
+		/// Deletes the specified payee.
+		/// </summary>
+		/// <param name="payeeId">The payee id.</param>
+		/// <returns>A <see cref="Task{TResult}"/> representing the result of the asynchronous operation.</returns>
+		[HttpDelete]
+		[Route("{payeeId}")]
+		[ProducesResponseType((int)HttpStatusCode.NoContent)]
+		public async Task<IActionResult> DeletePayee(Guid payeeId)
+		{
+			var payee = await this.databaseContext.Payees.FirstOrDefaultAsync(p => p.Id == payeeId);
+
+			if (payee != null)
+			{
+				this.databaseContext.Payees.Remove(payee);
+				await this.databaseContext.SaveChangesAsync();
+			}
+
+			return this.NoContent();
+		}
+	}
+}
---
+++ budget-api/Api/Models/CreatePayeeRequest.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Farooq Mahmud
+
+#pragma warning disable CS8618
+namespace Api.Models
+{
+	/// <summary>
+	/// Encapsulates a create payee API request.
+	/// </summary>
+	public class CreatePayeeRequest
+	{
+		/// <summary>
+		/// Gets or sets the payee name.
+		/// </summary>
+		public string Name { get; set; }
+	}
+}
